f7e1b9d4e70079a7e9cd4bb94b2e82153a294bd8
Muhammad Ahmad Fouad <muhammad.a.foad@gmail.com>
2025-10-04 09:24:19 +0300

    minor change

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import Logo from "../assets/TransparentLogo.png";
-import "../CSS/Navbar.css";
+import "../CSS/navbar.css";
 
 export default function Navbar() {
   return (
