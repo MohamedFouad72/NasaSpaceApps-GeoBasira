c7b239011ba560c786b71a34172b905c4d5cb013
Muhammad Ahmad Fouad <muhammad.a.foad@gmail.com>
2025-10-14 22:27:58 +0300

    origins correction

--- Backend/script.py
+++ Backend/script.py
@@ -399,7 +399,8 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",  # optional
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://geobasira.earth/"
 ]
 
 app.add_middleware(
